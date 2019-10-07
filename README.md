b2ecd776bd8001c6239b3f9c6e92328fd84277a3
Jesús <jesusalxen@gmail.com>
2019-10-07 22:23:27 +0200

    Added update, insert and delete type querys. Also added orWhere() method

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
-# mysql-query-builder
+# Simple mysql query builder
 
 Class php for build mysql querys
 
 ## Status
 
-- For now only select querys can be created
-- Insert, update, delete querys will be added soon
+- Select, update, insert and delete querys can be builded
 - Join option will be added soon
 
 ## Installation
 
-Clone or download this repository and include or require the query.class.php to use it
+Clone or download this repository
+
+```terminal
+    git clone git@github.com:jesusgm/mysql-query-builder.git
+```
+
+and include or require the query.class.php to use it
 
 ```php
 include "query.class.php";
 ```
 
 ## Usage
+
+### Selecting data
 
 ```php
     // Include class
@@ -28,11 +35,98 @@ include "query.class.php";
     $query->select();   //Type of query: select() || update() || insert() || delete()
     $query->table("posts"); // Set the table
     $query->columns(array("id", "title", "slug"));  // Set the columns to return. Default *
-    $query->where(array("title LIKE '%keywordtofind%'"));   // Set query filter
+    $query->where(array("`title` LIKE '%keywordtofind%'"));   // Set query filter. Array elements will be concatenated by AND
+    $query->orWhere(array("`id` = 1", "`id` = 2"));   // Set query filter. Array elements will be concatenated by OR
     $query->limit("1,10");  // Set limit of results or paginate
     $query->order("date DESC"); // Set order of result
 
     echo $query->build();   // Build and returns a query string
+```
+
+Returns:
+
+```sql
+    SELECT `id, title, slug`
+    FROM `posts`
+    WHERE (title LIKE '%keywordtofind%') AND (`id` = 1 OR `id` = 2)
+    ORDER BY `date`
+    DESC LIMIT 1,10;
+```
+
+### Insert data
+
+```php
+    // Include class
+    include "query.class.php";
+
+    // Create instance
+    $query = new Query();
+
+    $query->insert(array("'slug'", "'content content'", "'tag1, tag2'", "'title post'", 1, 2));
+    $query->table("blog_posts");
+    $query->columns(array("slug", "content", "tags", "title", "author_id", "category"));
+
+    echo $query->build();
+```
+
+Returns
+
+```sql
+    INSERT INTO `blog_posts`
+        (`slug`, `content`, `tags`, `title`, `author_id`, `category`)
+    VALUES ('slug','content content','tag1, tag2','title post',1,2)
+    WHERE (`slug` = 'slugnew');
+```
+
+### Update data
+
+```php
+    // Include class
+    include "query.class.php";
+
+    // Create instance
+    $query = new Query();
+
+    $query->update(array("'slugnew'", "'New title'"));
+    $query->table("blog_posts");
+    $query->columns(array("slug", "title"));
+    $query->where(array("`slug` = 'prueba'"));
+    $query->orWhere(array("`id` = 7", "`id` = 4"));
+
+    echo $query->build();
+```
+
+Returns
+
+```sql
+    UPDATE `blog_posts`
+    SET `slug`='slugnew', `title`='New title'
+    WHERE (`slug` = 'prueba') AND (`id` = 7 OR `id` = 4);
+```
+
+### Delete data
+
+```php
+    // Include class
+    include "query.class.php";
+
+    // Create instance
+    $query = new Query();
+
+    $query->delete();
+    $query->table("blog_posts");
+    $query->columns(array("id"));
+    $query->where(array("`slug` = 'slugnew'"));
+
+    echo $query->build();
+```
+
+Returns
+
+```sql
+    DELETE
+    FROM `blog_posts`
+    WHERE (`slug` = 'slugnew');
 ```
 
 ## License
--- query.class.php
+++ query.class.php
@@ -9,7 +9,9 @@
         var $type = "";
         var $table = "";
         var $columns = [];
+        var $values = [];
         var $where = [];
+        var $orWhere = [];
         var $order = "";
         var $limit = "";
         
@@ -17,11 +19,17 @@
 			$this->type = SELECT;
         }
 
-        function update() {
+        function update($values) {
             $this->type = UPDATE;
+            $this->values = $values;
         }
-        function insert() {
+        function insert($values) {
             $this->type = INSERT;
+            $this->values = $values;
+        }
+
+        function delete() {
+            $this->type = DELETE;
         }
         
         function table($table) {
@@ -34,6 +42,10 @@
 
         function where($where) {
             $this->where = $where;
+        }
+
+        function orWhere($orWhere) {
+            $this->orWhere = $orWhere;
         }
 
         function order($order) {
@@ -49,23 +61,52 @@
             $queryString = "";
             switch($this->type) {
                 case SELECT:
-                $queryString = "SELECT ";
-                if(count($this->columns)) {
-                    $queryString .= implode($this->columns, ", ");
-                } else {
-                    $queryString .= "*";
-                }
+                    $queryString = "SELECT ";
+                    if(count($this->columns)) {
+                        $queryString .= "`" . implode($this->columns, ", ") . "`";
+                    } else {
+                        $queryString .= "*";
+                    }
 
-                $queryString .= " FROM `".$this->table."`";
-                break;
+                    $queryString .= " FROM `".$this->table."`";
+                    break;
+                case INSERT:
+                    $queryString = "INSERT INTO `".$this->table."` (`".implode($this->columns, "`, `")."`)";
+                    $queryString .= " VALUES (".implode($this->values, ",").") ";
+                    break;
+                case UPDATE:
+                    $queryString = "UPDATE `".$this->table."`";
+                    $queryString .= " SET ";
+                    
+                    $map = array();
+                    for($i = 0; $i < count($this->columns); $i++){
+                        $map[] = "`".$this->columns[$i] . "`=" . $this->values[$i];
+                    }
+
+                    $queryString .= implode($map, ", ");
+                    break;
+                case DELETE:
+                    $queryString = "DELETE FROM `".$this->table."`";
+                    break;
+            }
+
+            if(count($this->where) || count($this->orWhere)){
+                $queryString .= " WHERE ";
             }
 
             if(count($this->where)){
-                $queryString .= " WHERE " . implode($this->where, " AND ");
+                $queryString .= "(" . implode($this->where, " AND ") .")";
+            }
+
+            if(count($this->orWhere)){
+                if(count($this->where)){
+                    $queryString .= " AND ";
+                }
+                $queryString .= "(" . implode($this->orWhere, " OR ") . ")";
             }
 
             if($this->order != ""){
-                $queryString .= " ORDER " . $this->order;
+                $queryString .= " ORDER BY " . $this->order;
             }
 
             if($this->limit != ""){
@@ -73,7 +114,7 @@
             }
 
 
-            return $queryString;
+            return $queryString . ";";
         }
         
 	}
